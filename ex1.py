9c41d7c18529851ea41132fcbec77332680c5027
Jeff Wright <jeff.washcloth@gmail.com>
2014-10-29 22:03:35 -0400

    Initial commit of semi-working code

---
+++ ex1.py
@@ -0,0 +1,46 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Ex 1:
+
+Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+Extras:
+
+(1) Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+
+(2) Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+'''
+
+from datetime import date
+
+def main():
+	name = get_name()
+	age = get_age()
+	year = get_year(int(age))
+	
+	if year == 'greater':
+		print ("You're already 100 years old or greater! Congrats!")
+	else:
+		print ('Hello, {}!'.format(name))
+		print ('You will turn 100 in the year {}.'.format(year))
+
+def get_name():
+	return (input('Please enter your name:'))
+
+def get_age():
+	return (input('Please enter your age:'))
+	
+def get_year(a):
+	assert a >= 0, "Age is not allowed to be less than zero."
+	if a >= 100:
+		return 'greater'
+	else:
+		today = str(date.today()).split('-')
+		# print (today)
+		# data of interest is in today[0]
+		return None
+		# Note: need to figure out algorithm
+		
+if __name__ == "__main__":
+	main()
