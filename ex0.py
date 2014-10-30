703022fa2866a6e323882d5516a51c72344207fb
Jeff Wright <jeff.washcloth@gmail.com>
2014-10-30 14:31:09 -0400

    Added in ex2.py, and ex0.py, which is a generic example template

---
+++ ex0.py
@@ -0,0 +1,12 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Comments go here
+So do doctests
+'''
+
+def main():
+	pass
+		
+if __name__ == "__main__":
+	main()
---
+++ ex2.py
@@ -0,0 +1,18 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Ex 2:
+
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+If the number is a multiple of 4, print out a different message.
+Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+'''
+
+def main():
+	pass
+		
+if __name__ == "__main__":
+	main()
