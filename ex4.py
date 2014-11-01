5d462dfa1ca8109cf1261156b5369157422f1535
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-01 09:21:13 -0600

    Implement code for ex4.py

--- ex4.py
+++ ex4.py
@@ -4,8 +4,21 @@
 Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 '''
 
+def get_divisors(n):
+	divs = []
+	count = 1
+	while count <= n:
+		if n % count == 0:
+			divs.append(count)
+		count += 1
+	return divs
+
 def main():
-	pass
+	integer = input("Please input a positive integer: ")
+	num = abs(int(float(integer)))
+	divisors = get_divisors(num)
+	for elem in divisors:
+		print(elem)
 		
 if __name__ == "__main__":
 	main()
