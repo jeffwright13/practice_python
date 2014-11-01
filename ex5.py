10590919807a776dc9f7ba983523b25592db4d02
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-01 09:24:15 -0600

    Add in stub code for ex5.py

---
+++ ex5.py
@@ -0,0 +1,20 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Take two lists, say for example these two:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+Extras:
+
+Randomly generate two lists to test this
+Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+'''
+
+def main():
+	pass
+		
+if __name__ == "__main__":
+	main()
