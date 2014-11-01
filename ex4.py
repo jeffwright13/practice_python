7bc262bf11dcaaf7c262f2ae50e8fe65bc6c2a14
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-01 09:03:36 -0600

    Add in ex4.py stub code

---
+++ ex4.py
@@ -0,0 +1,11 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+'''
+
+def main():
+	pass
+		
+if __name__ == "__main__":
+	main()
