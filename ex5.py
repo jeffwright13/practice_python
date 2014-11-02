69b9057acb70fdc7bc3f56aeb23b91f5e609bcb9
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-02 15:53:04 -0700

    Initial implementation

--- ex5.py
+++ ex5.py
@@ -4,7 +4,7 @@
 Take two lists, say for example these two:
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
 
 Extras:
@@ -14,7 +14,21 @@ Write this in one line of Python (don’t worry if you can’t figure this out a
 '''
 
 def main():
-	pass
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+	print (common(a, b))
+	
+def common(a, b):
+	'''
+	Returns a list that contains all common elements in two lists
+	passed into the function.
+	'''
+	c = []
+	for elem in a:
+		if elem in b:
+			if elem not in c:
+				c.append(elem)
+	return c
 		
 if __name__ == "__main__":
 	main()
