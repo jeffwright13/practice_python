d8c36da802149e1a437cf4b0714034b60b5db10e
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-04 10:04:52 -0500

    Initial stub for Ex 6

---
+++ ex6.py
@@ -0,0 +1,13 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Ask the user for a string and print out whether this string is a
+palindrome or not. (A palindrome is a string that reads the same
+forwards and backwards.)
+'''
+
+def main():
+	pass
+		
+if __name__ == "__main__":
+	main()
