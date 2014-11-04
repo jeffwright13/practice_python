bb64281e7da8e779b40e3e7b615fd4d253e5075e
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-04 13:48:26 -0500

    Add in stub for Ex 7

---
+++ ex7.py
@@ -0,0 +1,13 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line
+of Python that takes this list a and makes a new list that has only
+the even elements of this list in it. 
+'''
+
+def main():
+	pass
+		
+if __name__ == "__main__":
+	main()
