e0ef5d77e8514f9d90738a8f47e7fffcc79fb678
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-07 12:53:07 -0500

    Ex 9 initial implementation

---
+++ ex9.py
@@ -0,0 +1,17 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. (Hint: remember to use the user input lessons from the very first exercise)
+
+Extras:
+
+Keep the game going until the user types “exit”
+Keep track of how many guesses the user has taken, and when the game ends, print this out.'''
+
+import sys, random
+
+def main():
+	pass
+	
+if __name__ == "__main__":
+	main()
