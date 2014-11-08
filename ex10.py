9ceaaa8278aa11b04e3a28b27e140d152670a485
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-08 10:20:01 -0500

    Stub implementation of common() function

--- ex10.py
+++ ex10.py
@@ -4,17 +4,23 @@
 This week’s exercise is going to be revisiting an old exercise (see Exercise 5), except require the solution in a different way.
 
 Take two lists, say for example these two:
-
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes. Write this in one line of Python. (Hint: Remember list comprehensions from Exercise 7).
 
-Extra:
-
-Randomly generate two lists to test this
+Extra: Randomly generate two lists to test this
 '''
 
 def main():
+	import random
+	
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+	
+	l = common(a, b)
+	print (l)
+
+def common(l1, l2):
 	pass
 
 if __name__ == "__main__":
