607b0f1ff1e6ed6b0f4efd84a17893373e52f01f
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-07 21:14:12 -0500

    Ex10 stub

---
+++ ex10.py
@@ -0,0 +1,21 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+This week’s exercise is going to be revisiting an old exercise (see Exercise 5), except require the solution in a different way.
+
+Take two lists, say for example these two:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes. Write this in one line of Python. (Hint: Remember list comprehensions from Exercise 7).
+
+Extra:
+
+Randomly generate two lists to test this
+'''
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
