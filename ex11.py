d5d0754cc83697d2e11ed07cd43fb8204189f88f
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-13 14:14:04 -0500

    Added in stub for Ex 11

---
+++ ex11.py
@@ -0,0 +1,11 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Ask the user for a number and determine whether the number is prime or not. (For those who have forgotten, a prime number is a number that has no divisors.). You can (and should!) use your answer to [Exercise 4](/exercise/2014/02/26/04-divisors.html to help you. Take this opportunity to practice using functions, described below.
+'''
+
+def main():
+	pass
+	
+if __name__ == "__main__":
+	main()
