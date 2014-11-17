2ee964ef30dea18349ea7c416cbb87089061ff2d
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-17 08:32:55 -0500

    Implementation of Fib function for Ex 13

--- ex13.py
+++ ex13.py
@@ -11,13 +11,18 @@ looks like this: 1, 1, 2, 3, 5, 8, 13, ...)
 '''
 
 def main():
-	n = input("How many numbers in your Fibonacci sequence? ")
+	n = int(float(input("How many in your Fibonacci sequence? ")))
 	print (fibonacci(n))
 	
 def fibonacci(n):
-	fib = []
-	if n == 0: return fib
-	pass
+	if n <= 0: return []
+	elif n == 1: return [1]
+	else:
+		fib = [1, 1]
+		for x in range(2, n):
+			fib.append(fib[x-2] + fib[x-1])
+			print (fib)
+		return fib
 	
 if __name__ == "__main__":
 	main()
