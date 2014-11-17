ac2b7661cb3d5ac7b71d4c3cdd2129a90fb0e7ec
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-17 08:35:09 -0500

    Stub implementation of Ex 14

---
+++ ex14.py
@@ -0,0 +1,16 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
+Extras:
+
+Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+'''
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
