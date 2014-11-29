493a64234d47f18d013186c46dbf1fbcc499bde2
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-29 09:54:56 -0700

    Ex 16 stub implementation

---
+++ ex16.py
@@ -0,0 +1,16 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random, generating a new password every time the user asks for a new password. Include your run-time code in a main method.
+
+Extra: Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
+'''
+
+def main():
+	pass
+	
+def generate_password(seed, strength):
+	pass
+	
+if __name__ == "__main__":
+	main()
