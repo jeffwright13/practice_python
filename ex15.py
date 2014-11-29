f8a46d306099ba107855fcfb8bcf7d1fb3835db2
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-28 18:09:54 -0700

    Stub for Ex 15

---
+++ ex15.py
@@ -0,0 +1,14 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
+'''
+Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string "My name is Michele", then the result would be "Michele is name My".
+'''
+
+def main():
+	pass
+
+def reverse_words(str):
+	pass
+	
+if __name__ == "__main__":
+	main()
