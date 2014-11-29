f0906a98f191021831eeb43332ecf7b0a62810d9
Jeff Wright <jeff.washcloth@gmail.com>
2014-11-28 18:12:25 -0700

    Next steps...

--- ex15.py
+++ ex15.py
@@ -1,13 +1,14 @@
 #!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
 
 '''
-Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string "My name is Michele", then the result would be "Michele is name My".
+Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in reverse order. For example, say I type the string "My name is Michele", then the result would be "Michele is name My".
 '''
 
 def main():
-	pass
+	sentence = input('Enter a sentence:')
+	print (reverse(sentence))
 
-def reverse_words(str):
+def reverse(str):
 	pass
 	
 if __name__ == "__main__":
