dd2dea351083ced5229f562fa1720af58caa758a
Jeff Wright <jeff.washcloth@gmail.com>
2015-04-13 14:56:26 -0400

    Final version of Ex 17 (works)

--- ex17.py
+++ ex17.py
@@ -4,14 +4,19 @@
 Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage (http://www.nytimes.com/)
 '''
 
-import BeautifulSoup
-import requests
-
 def main():
-	pass
+    from bs4 import BeautifulSoup
+    import requests
+    
+    url = 'http://www.nytimes.com/'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    
+    title = soup.find_all(class_ = 'story-heading')
+    print title
 
 def something():
     pass
 
 if __name__ == "__main__":
-	main()
+    main()
