fb5e9207d437c0e448086e06f1087180c6e01f01
Jeff Wright <jeff.washcloth@gmail.com>
2015-04-13 08:48:03 -0400

    Add in stub for Ex 17

---
+++ ex17.py
@@ -0,0 +1,17 @@
+#!/c/Python27/python.exe
+
+'''
+Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage (http://www.nytimes.com/)
+'''
+
+import BeautifulSoup
+import requests
+
+def main():
+	pass
+
+def something():
+    pass
+
+if __name__ == "__main__":
+	main()
